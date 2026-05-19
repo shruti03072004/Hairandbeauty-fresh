ea0b2b1b54e8845823582baf15f5c09e6922b677
Shruti Dakhore <shrutidakhore@gmail.com>
2026-05-19 14:19:08 +0530

    removed razorpay build secret

--- salonbackend/src/main/java/com/salon/backend/security/JwtAuthenticationFilter.java
+++ salonbackend/src/main/java/com/salon/backend/security/JwtAuthenticationFilter.java
@@ -1,17 +1,19 @@
 package com.salon.backend.security;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-import java.util.Collections;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+//shruti
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
